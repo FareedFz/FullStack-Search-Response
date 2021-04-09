2765407df901b260c777f6b6242c19ea1b460f68
FareedFz <78958186+FareedFz@users.noreply.github.com>
2021-04-09 20:48:34 +0530

    Add files via upload

---
+++ loopcheck.js
@@ -0,0 +1,13 @@
+var changeArr =[];
+var arr =["jaffer","shaule"];
+console.log(arr);
+for(var i=0;i<arr.length;i++){
+	var name = arr[i]
+    changeArr[name] =arr[i]
+    
+}
+
+
+
+console.log(changeArr);
+console.log(changeArr['jaffer']);
