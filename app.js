37814a0ef8f91541eb12dd71f9dccbed3d014d33
FareedFz <78958186+FareedFz@users.noreply.github.com>
2021-04-09 20:47:50 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express=require('express')
+var app=express();
+var router = require('./router');
+let port=6200;
+app.all('*',router)
+             app.listen(port, () => {
+                console.log(`Running Successfully`);
+                  })
+   
+             
+                  
+        
+module.exports=app;
